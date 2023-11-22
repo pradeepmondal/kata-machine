cc76a1f59223cfe561550a7b85eb2ccdc91deb8f
pradeepmondal <mail.pradeepmondal@gmail.com>
2023-11-22 21:22:09 +0530

    done Two Crystals Problem

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,17 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jmpSize = Math.floor(Math.sqrt(breaks.length))
+    let curIdx = 0;
+    while(curIdx < breaks.length){
+        if((breaks[curIdx + jmpSize] === true) || (curIdx+jmpSize) >= breaks.length) {
+            for (let i = curIdx; i < curIdx+jmpSize; i++){
+                if(breaks[i] === true){
+                    return i;
+                }
+            }
+        }
+        curIdx += jmpSize
+    }
+    return -1;
+
 
 }
